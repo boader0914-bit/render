c18290fe755d469676fe5e9d96ec239f320a5997
boader0914-bit <boader0914@gmail.com>
2026-06-21 11:54:20 +0900

    Restore crypto import for traffic metrics

--- scripts/glamping_app_server.cjs
+++ scripts/glamping_app_server.cjs
@@ -1,5 +1,6 @@
 const fs = require("node:fs");
 const fsp = require("node:fs/promises");
+const crypto = require("node:crypto");
 const http = require("node:http");
 const os = require("node:os");
 const path = require("node:path");
