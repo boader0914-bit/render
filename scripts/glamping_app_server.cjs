22e1b7ab29076866d7ea5c3812753a2cf1a10f8e
boader0914-bit <boader0914@gmail.com>
2026-06-30 09:57:26 +0900

    Bump company master cache version

--- scripts/glamping_app_server.cjs
+++ scripts/glamping_app_server.cjs
@@ -4136,8 +4136,8 @@ async function serveStatic(reqUrl, res) {
   if (reqUrl.pathname === "/" || reqUrl.pathname === "/view") {
     const html = await fsp.readFile(path.join(WEB_DIR, "index.html"), "utf8");
     const publicHtml = html
-      .replace('href="/styles.css"', 'href="/styles.css?v=v2-20260630-company-master"')
-      .replace('src="/app.js"', 'src="/app.js?v=v2-20260630-company-master"');
+      .replace('href="/styles.css"', 'href="/styles.css?v=v2-20260630-company-master-correction"')
+      .replace('src="/app.js"', 'src="/app.js?v=v2-20260630-company-master-correction"');
     return send(res, 200, publicHtml, "text/html; charset=utf-8");
   }
   const filePath = safeJoin(WEB_DIR, reqUrl.pathname);
